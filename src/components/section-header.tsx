8513200c86fe6a09a10b098f8d041b49b040a98b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-13 11:23:47 +0300

    refactor: add id prop

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -1,15 +1,20 @@
 interface SectionHeaderProps {
   header: string;
   descripton: string;
+  id: string;
 }
 
 export default function SectionHeader({
   descripton,
   header,
+  id,
 }: SectionHeaderProps) {
   return (
     <div className="space-y-2 text-center">
-      <h3 className="font-mono text-xl md:text-2xl font-semibold text-secondary">
+      <h3
+        className="font-mono text-xl md:text-2xl font-semibold text-secondary"
+        id={id}
+      >
         {header}
       </h3>
       <p className="text-base max-w-prose mx-auto">{descripton}</p>
