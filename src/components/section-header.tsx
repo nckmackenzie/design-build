f0e23e7cb1341825d7c88ce92405b285c3f3ac19
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-13 15:38:11 +0300

    refactor: ability to pass classes for desc

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -1,13 +1,17 @@
+import { cn } from '@/lib/utils';
+
 interface SectionHeaderProps {
   header: string;
   descripton: string;
   id: string;
+  descriptionClassName?: string;
 }
 
 export default function SectionHeader({
   descripton,
   header,
   id,
+  descriptionClassName,
 }: SectionHeaderProps) {
   return (
     <div className="space-y-2 text-center">
@@ -17,7 +21,9 @@ export default function SectionHeader({
       >
         {header}
       </h3>
-      <p className="text-base max-w-prose mx-auto">{descripton}</p>
+      <p className={cn('text-base max-w-prose mx-auto', descriptionClassName)}>
+        {descripton}
+      </p>
     </div>
   );
 }
