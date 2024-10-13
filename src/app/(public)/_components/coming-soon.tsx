09f938fb8cf7afb5bf5a57c0a044441604985f34
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-13 15:51:34 +0300

    chore: dummy placeholder component for page

---
+++ src/app/(public)/_components/coming-soon.tsx
@@ -0,0 +1,12 @@
+export default function ComingSoon() {
+  return (
+    <div className="min-h-[400px] grid place-content-center bg-background text-foreground p-4">
+      <div className="text-center">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">Coming Soon</h1>
+        <p className="text-xl md:text-2xl mb-8">
+          We're working hard to bring you something amazing!
+        </p>
+      </div>
+    </div>
+  );
+}
