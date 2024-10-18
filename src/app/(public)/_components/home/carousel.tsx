92006f5e4bb408972f4cf289dce7752db2ebaa55
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-18 16:07:37 +0300

    feat: carousel for projects

---
+++ src/app/(public)/_components/home/carousel.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+export default function ProjectCarousel() {
+  return (
+    <Carousel autoPlay infiniteLoop showThumbs={false}>
+      <div>
+        <img src="/home/carousel_1.jpg" alt="" />
+      </div>
+      <div>
+        <img src="/home/carousel_2.jpg" alt="" />
+      </div>
+      <div>
+        <img src="/home/carousel_3.jpg" alt="" />
+      </div>
+      <div>
+        <img src="/home/carousel_4.jpg" alt="" />
+      </div>
+      <div>
+        <img src="/home/carousel_5.jpg" alt="" />
+      </div>
+    </Carousel>
+  );
+}
