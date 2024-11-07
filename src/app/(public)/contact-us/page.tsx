48833b102d429fe264300ea99d8dce1316ba3846
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-11-07 12:59:37 +0300

    feat: contact us details including form for reaching out

--- src/app/(public)/contact-us/page.tsx
+++ src/app/(public)/contact-us/page.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
-import ComingSoon from '../_components/coming-soon';
+import { type Metadata } from 'next';
+
+import SectionHeader from '@/components/section-header';
+import ContactForm from '../_components/contact/contact-form';
+import ContactUsOther from '../_components/contact/contact-other';
+
+export const metadata: Metadata = {
+  title: 'Contact Us',
+};
 
 export default function ContactUsPage() {
-  return <ComingSoon />;
+  return (
+    <section
+      className="pb-12 md:pb-16 pt-8 md:pt-12 mx-4 rounded-3xl space-y-6 container"
+      aria-labelledby="contact-us"
+    >
+      <SectionHeader
+        id="contact-us"
+        header="Contact our team"
+        descripton="Have questions or ready to start your project? We’re here to help! Reach out to our team for expert guidance, personalized solutions, or to discuss your unique vision. Let’s bring your ideas to life together."
+      />
+      <div className="grid gap-4 sm:gap-x-8 sm:grid-cols-12">
+        <div className="sm:col-span-8">
+          <ContactForm />
+        </div>
+        <ContactUsOther />
+      </div>
+    </section>
+  );
 }
