c9096f0d6589772c726f2b469115efc69b05bdb8
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-11-18 15:22:57 +0300

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -192,7 +192,7 @@ start developing a clear roadmap for your project’s success. Let’s build som
   },
   {
     question: 'Our Process and Services',
-    answer: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries`,
+    answer: `Our approach is designed to deliver efficiency, quality, and satisfaction at every stage. We begin with a thorough consultation to understand your needs and create a tailored plan. From bespoke architectural design and meticulous site planning to expert construction management and interior detailing, we handle every aspect with precision. Additionally, we provide high-quality construction materials, custom furniture, interior elements, and essential household equipment. Our mission is to offer a seamless, integrated experience that transforms your vision into reality.`,
   },
 
   {
