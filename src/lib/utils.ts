ada8c377975f935dfcadab64280e7e3a59c93d04
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-12-08 18:46:58 +0300

    chore: util fn for converting to title case

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const titleCase = (str: string) => {
+  return str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+};
